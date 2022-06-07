4f29d9816c753884d5b3406e2f5ed02d7e8e14bd
Akshay Mahadeva <akshay.mahadev94@gmail.com>
2022-06-07 12:57:01 +0530

    changed data

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
-    email: 'janedoe.test@gmail.com',
-    phone: '+915588776600',
-    address: 'Menlo Park, California, United States - 673822 ',
+    email: 'akshay.mahadev94@gmail.com',
+    phone: '+916364811912',
+    address: 'Mysore, Karnataka, India',
 
     sheetAPI: ''
 }
--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,23 @@
 export const educationData = [
-    {
-        id: 1,
-        institution: 'Marble Hills Middle School',
-        course: 'Higher Secondary Education',
-        startYear: '2007',
-        endYear: '2015'
-    },
+    // {
+    //     id: 1,
+    //     institution: 'Marble Hills Middle School',
+    //     course: 'Higher Secondary Education',
+    //     startYear: '2007',
+    //     endYear: '2015'
+    // },
     {
         id: 2,
-        institution: 'Somerset School of Science',
-        course: 'Bachelor of Technology',
-        startYear: '2015',
-        endYear: '2019'
+        institution: 'Visvesvaraya Technological University, India',
+        course: 'Bachelor of Engineering',
+        startYear: '2012',
+        endYear: '2016'
     },
     {
         id: 3,
-        institution: 'Hawking University, Cambridge',
-        course: 'Master of Technology',
-        startYear: '2019',
-        endYear: 'Present'
+        institution: 'Chalmers University of Technology, Sweden',
+        course: 'Master of Science',
+        startYear: '2017',
+        endYear: '2020'
     },
 ]
